71523d8f57ba197e7cab3f15c07c1ee1c55840a5
ndzib <brunoserkwi@gmail.com>
2019-08-21 11:40:57 +0100

    modified the database, to ensure that when a category is deleted, all products of that category are deleted

--- sql_script/product_catalog_ysql_script.sql
+++ sql_script/product_catalog_ysql_script.sql
@@ -1,28 +1,48 @@
 CREATE DATABASE productCatalog;
-USE productCatalog;
 
-CREATE TABLE category(
-	categoryID int AUTO_INCREMENT,
-	categoryName varchar(20),
-	categoryDescription varchar (100),
-	PRIMARY KEY(categoryID)
+CREATE TABLE `category` (
+  `categoryID` int(11) NOT NULL,
+  `categoryName` varchar(20) DEFAULT NULL,
+  `categoryDescription` varchar(100) DEFAULT NULL
 );
 
-CREATE TABLE product (
-	categoryID int NOT NULL,
-	productID int AUTO_INCREMENT,
-	productName varchar(20),
-	productDescription varchar(100),
-	productColor varchar(10),
-	
-	PRIMARY KEY(productID),
-	FOREIGN KEY (categoryID) REFERENCES category(categoryID)
-);
+CREATE TABLE `product` (
+  `categoryID` int(11) NOT NULL,
+  `productID` int(11) NOT NULL,
+  `productName` varchar(20) DEFAULT NULL,
+  `productDescription` varchar(100) DEFAULT NULL,
+  `productColor` varchar(10) DEFAULT NULL
+) ;
 
-CREATE TABLE systemUser (
-	userID integer AUTO_INCREMENT,
-	userRealName varchar(30) NOT NULL,
-	userName varchar(10),
-	userPassword char(60) NOT NULL,
-	PRIMARY KEY(userID)
-);
+INSERT INTO `category` (`categoryID`, `categoryName`, `categoryDescription`) VALUES
+(1, 'Kitchen', 'Kitchen utensils'),
+(2, 'Agriculture', 'Farming tools'),
+(13, 'drink', 'Kitchen utensils');
+INSERT INTO `product` (`categoryID`, `productID`, `productName`, `productDescription`, `productColor`) VALUES
+(1, 2, 'Pan', 'shallow plate-like vessel for frying food-stuff', 'Black'),
+(13, 7, 'planet orange', 'shallow plate-like vessel for frying food-stuff', 'Green');
+CREATE TABLE `systemuser` (
+  `userID` int(11) NOT NULL,
+  `userRealName` varchar(30) NOT NULL,
+  `userName` varchar(10) DEFAULT NULL,
+  `userPassword` char(60) NOT NULL
+) ;
+INSERT INTO `systemuser` (`userID`, `userRealName`, `userName`, `userPassword`) VALUES
+(1, 'Bruno Nzi', 'ndzib', 'bruno'),
+(2, 'Serkwi Bruno Ndzi', 'ndzib', 'ndzib');
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categoryID`);
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`productID`),
+  ADD KEY `categoryID` (`categoryID`);
+ALTER TABLE `systemuser`
+  ADD PRIMARY KEY (`userID`);
+ALTER TABLE `category`
+  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+ALTER TABLE `product`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `systemuser`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `category` (`categoryID`) ON DELETE CASCADE;
+
